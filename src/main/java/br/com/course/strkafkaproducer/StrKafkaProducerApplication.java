1dfb1905ce49098639e1b29c264d3447de9ca272
anonymous <jcolivati@gmail.com>
2023-08-10 10:38:50 -0300

    big bang

---
+++ src/main/java/br/com/course/strkafkaproducer/StrKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.course.strkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/course/strkafkaproducer/StrKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.strkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
