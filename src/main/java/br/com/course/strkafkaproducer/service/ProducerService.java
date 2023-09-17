a692bc63b4499a1f23a150a46dcd717e5acb9e41
anonymous <jcolivati@gmail.com>
2023-09-17 19:37:57 -0300

    feat: send message

---
+++ src/main/java/br/com/course/strkafkaproducer/service/ProducerService.java
@@ -0,0 +1,18 @@
+package br.com.course.strkafkaproducer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic",message);
+    }
+
+
+}
